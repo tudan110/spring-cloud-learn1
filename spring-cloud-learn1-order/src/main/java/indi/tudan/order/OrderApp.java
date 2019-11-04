70fb108bf1aa0d47647f6665d1265af31aef9016
tudan110 <wtudan@126.com>
2019-11-04 10:53:39 +0800

    --

---
+++ spring-cloud-learn1-order/src/main/java/indi/tudan/order/OrderApp.java
@@ -0,0 +1,32 @@
+package indi.tudan.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrix
+@ComponentScan(basePackages = {"indi.tudan.order.controller", "indi.tudan.order.service","indi.tudan.order.properties"})//手动指定bean扫描范围
+public class OrderApp {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApp.class, args);
+    }
+
+    /**
+     * 向Spring容器中定义RestTemplate对象
+     * @return
+     */
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
+    }
+
+}
